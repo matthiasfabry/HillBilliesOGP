a2426e27935122d1d21d8f4cf85c289a482a0fc8
Lukas Van Riel <lukasvr96@icloud.com>
2016-05-14 13:59:31 +0200

    fixing corrupted files

--- OGP1516-Hillbillies/src/hillbillies/model/coordinateexpression/SpecifiedExpression.java
+++ OGP1516-Hillbillies/src/hillbillies/model/coordinateexpression/SpecifiedExpression.java
@@ -1,6 +1,5 @@
 package hillbillies.model.coordinateexpression;
 
-import hillbillies.model.Coordinate;
 import hillbillies.model.expression.PositionExpression;
 
 public class SpecifiedExpression extends PositionExpression<Coordinate> {
