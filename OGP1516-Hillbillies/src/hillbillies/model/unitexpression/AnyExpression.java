9b68dceed4ace91138ba7ec3b610a0175b9ebd05
Lukas Van Riel <lukasvr96@icloud.com>
2016-05-14 15:06:15 +0200

    implemented anyUnitExpression

--- OGP1516-Hillbillies/src/hillbillies/model/unitexpression/AnyExpression.java
+++ OGP1516-Hillbillies/src/hillbillies/model/unitexpression/AnyExpression.java
@@ -1,6 +1,11 @@
 package hillbillies.model.unitexpression;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import hillbillies.model.Coordinate;
 import hillbillies.model.Unit;
+import hillbillies.model.World;
 import hillbillies.model.expression.UnitExpression;
 
 public class AnyExpression extends UnitExpression<Unit> {
@@ -18,7 +23,28 @@ public class AnyExpression extends UnitExpression<Unit> {
 	}
 
 	public Unit determineAnyUnit(){
-		return null;
+		Coordinate position = thisUnit.getInWorldPosition();
+		World world = thisUnit.getWorld();
+		Unit someUnit = null;
+		Coordinate[] coordinatelist = position.adjacentCoordinates();
+		Set<Coordinate> coordinateSet = new HashSet<Coordinate>();
+		for (Coordinate c: coordinatelist){
+			coordinateSet.add(c);
+		}
+		while (someUnit == null){
+			for (Unit worldUnit: world.getUnitSet()){
+				if (coordinateSet.contains(worldUnit.getInWorldPosition()))
+					someUnit = worldUnit;
+			}
+			for (Coordinate cc: coordinateSet){
+				Coordinate[] clist = cc.adjacentCoordinates();
+				for (Coordinate ccc: clist){
+					if (! coordinateSet.contains(ccc))
+						coordinateSet.add(ccc);
+				}
+			}	
+		}	
+		return someUnit;
 	}
 
 }
