3e68db4947a20b0d6e2218365dab9d0132b5fcbb
Lukas Van Riel <lukasvr96@icloud.com>
2016-05-15 23:21:16 +0200

    implemented PassableExpression

--- OGP1516-Hillbillies/src/hillbillies/model/expression/AndExpression.java
+++ OGP1516-Hillbillies/src/hillbillies/model/expression/AndExpression.java
@@ -8,7 +8,7 @@ package hillbillies.model.expression;
  * @param <T>
 *
 */
-public class AndExpression<T> implements Expression<T> {
+public class AndExpression<T> implements Expression<Expression[]> {
 
 	public AndExpression(Expression<T> firstExpression, Expression<T> secondExpression){
 		this.first = firstExpression;
@@ -16,9 +16,11 @@ public class AndExpression<T> implements Expression<T> {
 	}
 	
 	@Override
-	public T evaluate() {
-		return first.evaluate();
-		//return second.evaluate();
+	public Expression[] evaluate() {
+		Expression[] list = Expression[2];
+		list[0] = first;
+		list[1] = second;
+		return list;
 	}
 	private final Expression<T> first;
 	private final Expression<T> second;
--- OGP1516-Hillbillies/src/hillbillies/model/expression/Is_PassableExpression.java
+++ OGP1516-Hillbillies/src/hillbillies/model/expression/Is_PassableExpression.java
@@ -19,5 +19,4 @@ public class Is_PassableExpression implements Expression<Boolean> {
 		else 
 			return false;
 	}
-
 }
