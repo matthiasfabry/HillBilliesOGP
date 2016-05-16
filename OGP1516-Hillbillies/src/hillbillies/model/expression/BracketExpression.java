93c4e2536d9a19404c2810a23005d80c7671566a
Lukas Van Riel <lukasvr96@icloud.com>
2016-05-16 13:28:54 +0200

    added bracketExpression

---
+++ OGP1516-Hillbillies/src/hillbillies/model/expression/BracketExpression.java
@@ -0,0 +1,11 @@
+package hillbillies.model.expression;
+
+public class BracketExpression<T> implements Expression<T> {
+
+	@Override
+	public T evaluate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
